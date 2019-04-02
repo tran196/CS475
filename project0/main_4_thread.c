94d9c69b6f872af3f85617563d1ee68f9f991e33
Anthony <anthony92@gmail.com>
2019-04-02 14:45:08 -0700

    adding project 0

---
+++ project0/main_4_thread.c
@@ -0,0 +1,48 @@
+#include <omp.h>
+#include <stdio.h>
+#include <math.h>
+
+#define NUMT        4
+#define ARRAYSIZE   1000    //you decide
+#define NUMTRIES    100    	// you decide
+
+float A[ARRAYSIZE];
+float B[ARRAYSIZE];
+float C[ARRAYSIZE];
+
+int main( )
+{
+
+#ifndef _OPENMP
+        fprintf( stderr, "OpenMP is not supported here -- sorry.\n" );
+        return 1;
+#endif
+
+        omp_set_num_threads( NUMT );
+        fprintf( stderr, "Using %d threads\n", NUMT );
+
+        double maxMegaMults = 0.;
+
+        for( int t = 0; t < NUMTRIES; t++ )
+        {
+                double time0 = omp_get_wtime( );
+
+                #pragma omp parallel for
+                for( int i = 0; i < ARRAYSIZE; i++ )
+                {
+                        C[i] = A[i] * B[i];
+                }
+
+                double time1 = omp_get_wtime( );
+                double megaMults = (double)ARRAYSIZE/(time1-time0)/1000000.;
+                if( megaMults > maxMegaMults )
+                        maxMegaMults = megaMults;
+        }
+
+        printf( "Peak Performance = %8.2lf MegaMults/Sec\n", maxMegaMults );
+
+	// note: %lf stands for "long float", which is how printf prints a "double"
+	//        %d stands for "decimal integer", not "double"
+
+        return 0;
+}
---
+++ testTiming.c
@@ -0,0 +1,9 @@
+//From Project Notes Handout
+//How Reliable is the Timing PDF Page 6
+//
+
+
+double maxmflops = 0;
+double summmflops = 0;
+
+for (int t = 0; t < NUMTRIES
