25cb92f53f4c244302212f12fc4ce4197724cec8
tran196 <anthony92@gmail.com>
2019-05-20 12:03:45 -0700

    uncommenting code on lines 212-215

--- project5/first.cpp
+++ project5/first.cpp
@@ -209,10 +209,10 @@ main( int argc, char *argv[ ] )
 		float expected = hA[i] * hB[i];
 		if( fabs( hC[i] - expected ) > TOL )
 		{
-			//fprintf( stderr, "%4d: %13.6f * %13.6f wrongly produced %13.6f instead of %13.6f (%13.8f)\n",
-				//i, hA[i], hB[i], hC[i], expected, fabs(hC[i]-expected) );
-			//fprintf( stderr, "%4d:    0x%08x *    0x%08x wrongly produced    0x%08x instead of    0x%08x\n",
-				//i, LookAtTheBits(hA[i]), LookAtTheBits(hB[i]), LookAtTheBits(hC[i]), LookAtTheBits(expected) );
+			fprintf( stderr, "%4d: %13.6f * %13.6f wrongly produced %13.6f instead of %13.6f (%13.8f)\n",
+				i, hA[i], hB[i], hC[i], expected, fabs(hC[i]-expected) );
+			fprintf( stderr, "%4d:    0x%08x *    0x%08x wrongly produced    0x%08x instead of    0x%08x\n",
+				i, LookAtTheBits(hA[i]), LookAtTheBits(hB[i]), LookAtTheBits(hC[i]), LookAtTheBits(expected) );
 		}
 	}
 
