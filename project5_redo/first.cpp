124410639c41cfb78c9581e3f75571d482f804da
tran196 <anthony92@gmail.com>
2019-05-28 12:04:36 -0700

    adding a print statement to first.cpp

--- project5_redo/first.cpp
+++ project5_redo/first.cpp
@@ -11,8 +11,8 @@
 #endif
 #include <omp.h>
 
-#include "CL/cl.h"
-#include "CL/cl_platform.h"
+#include "cl.h"
+#include "cl_platform.h"
 
 
 #ifndef NMB
@@ -216,6 +216,7 @@ main( int argc, char *argv[ ] )
 		}
 	}
 
+    printf("NMB,\tLOCAL_SIZE,\tNUM_WORK_GROUPS,\tGigaMultsPerSecond\n");
 	fprintf( stderr, "%8d\t%4d\t%10d\t%10.3lf GigaMultsPerSecond\n",
 		NMB, LOCAL_SIZE, NUM_WORK_GROUPS, (double)NUM_ELEMENTS/(time1-time0)/1000000000. );
 
